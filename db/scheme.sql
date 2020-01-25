a08d8aa4b4d9df9794036f536029e0be1ab8250b
dgaujero <deion@aujero.net>
2020-01-25 11:20:22 -0500

    fixed

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE burgerse_db;
+CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
@@ -6,6 +6,7 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
+	devoured boolean,
 	PRIMARY KEY (id)
 );
 
